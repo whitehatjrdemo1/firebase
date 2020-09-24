3a7979a35566db278dfb82044105ce5ff45f4fea
whitehatjrdemo1 <whitehatjrdemo-1@yahoo.com>
2020-09-24 10:07:48 +0530

    changes

--- sketch.js
+++ sketch.js
@@ -1,29 +1,45 @@
 var ball;
-
+var hypball,database;
+var position;
 function setup(){
     createCanvas(500,500);
-    ball = createSprite(250,250,10,10);
-    ball.shapeColor = "red";
+    database=firebase.database();
+    console.log(database);
+    hypball = createSprite(250,250,10,10);
+    hypball.shapeColor = "red";
+    var hypballPosition=database.ref('ball/position');
+    hypballPosition.on("value",readPosition);
 }
 
 function draw(){
     background("white");
     if(keyDown(LEFT_ARROW)){
-        changePosition(-1,0);
+        writePosition(-1,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        changePosition(1,0);
+        writePosition(1,0);
     }
     else if(keyDown(UP_ARROW)){
-        changePosition(0,-1);
+        writePosition(0,-1);
     }
     else if(keyDown(DOWN_ARROW)){
-        changePosition(0,+1);
+        writePosition(0,+1);
     }
     drawSprites();
 }
 
 function changePosition(x,y){
-    ball.x = ball.x + x;
-    ball.y = ball.y + y;
+    hypball.x = hypball.x + x;
+    hypball.y = hypball.y + y;
+}
+function readPosition(data){
+    position=data.val();
+    hypball.x=position.x;
+    hypball.y=position.y
+}
+function writePosition(x,y){
+    database.ref('ball/postion').set({
+        'x':position.x+x,
+        'y':position.y+y
+    })
 }
